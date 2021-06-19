abb79d8107474a4683187b84282ae66b74d6fea7
Michel Jung <michel.jung89@gmail.com>
2021-06-19 11:43:28 +0200

    Fix build, go JUnit 5

--- src/test/java/com/github/nocatch/NoCatchTest.java
+++ src/test/java/com/github/nocatch/NoCatchTest.java
@@ -1,8 +1,6 @@
 package com.github.nocatch;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
@@ -10,11 +8,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static com.github.nocatch.NoCatch.noCatch;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NoCatchTest {
 
@@ -29,113 +26,113 @@ public class NoCatchTest {
     // Because it has no 'cause' constructor
   }
 
-  @Rule
-  public ExpectedException expectedException = ExpectedException.none();
-
   @Test
-  public void testNoCatchRunnableRunsFine() throws Exception {
+  public void testNoCatchRunnableRunsFine() {
     noCatch(() -> System.out.println(new URL("http://www.github.com")));
   }
 
   @Test
-  public void testNoCatchCallableRunsFine() throws Exception {
+  public void testNoCatchCallableRunsFine() {
     URL url = noCatch(() -> new URL("http://www.github.com"));
     assertThat(url.toString(), is("http://www.github.com"));
   }
 
   @Test
   public void testNoCatchRunnableDoesntWrapRuntimeException() {
-    expectedException.expect(IllegalStateException.class);
-
-    noCatch((NoCatch.NoCatchRunnable) () -> {
+    assertThrows(IllegalStateException.class, () -> noCatch((NoCatch.NoCatchRunnable) () -> {
       throw new IllegalStateException();
-    });
+    }));
   }
 
   @Test
   public void testNoCatchCallableDoesntWrapRuntimeException() {
-    expectedException.expect(IllegalStateException.class);
-
-    noCatch(() -> {
+    assertThrows(IllegalStateException.class, () -> noCatch(() -> {
       throw new IllegalStateException();
-    });
+    }));
   }
 
   @Test
-  public void testNoCatchRunnableWithCustomWrapperRunsFine() throws Exception {
+  public void testNoCatchRunnableWithCustomWrapperRunsFine() {
     noCatch(() -> System.out.println(new URL("http://www.github.com")), WrapperException.class);
   }
 
   @Test
-  public void testNoCatchCallableCustomWrapperRunsFine() throws Exception {
+  public void testNoCatchCallableCustomWrapperRunsFine() {
     URL url = noCatch(() -> new URL("http://www.github.com"), WrapperException.class);
     assertThat(url.toString(), is("http://www.github.com"));
   }
 
   @Test
-  public void testNoCatchRunnable() throws Exception {
-    expectedException.expect(RuntimeException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> System.out.println(new URL(";")));
+  public void testNoCatchRunnable() {
+    RuntimeException exception = assertThrows(
+      RuntimeException.class,
+      () -> noCatch(() -> System.out.println(new URL(";")))
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testNoCatchRunnableWrapperException() throws Exception {
-    expectedException.expect(WrapperException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> System.out.println(new URL(";")), WrapperException.class);
+  public void testNoCatchRunnableWrapperException() {
+    WrapperException exception = assertThrows(
+      WrapperException.class,
+      () -> noCatch(() -> System.out.println(new URL(";")), WrapperException.class)
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testNoCatchRunnableInvalidWrapperException() throws Exception {
-    expectedException.expect(NoCatchException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> System.out.println(new URL(";")), InvalidWrapperException.class);
+  public void testNoCatchRunnableInvalidWrapperException() {
+    NoCatchException exception = assertThrows(
+      NoCatchException.class,
+      () -> noCatch(() -> System.out.println(new URL(";")), InvalidWrapperException.class)
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testNoCatchCallable() throws Exception {
-    expectedException.expect(RuntimeException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> new URL(";"));
+  public void testNoCatchCallable() {
+    RuntimeException exception = assertThrows(
+      RuntimeException.class,
+      () -> noCatch(() -> new URL(";"))
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testNoCatchCallableWrapperException() throws Exception {
-    expectedException.expect(WrapperException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> new URL(";"), WrapperException.class);
+  public void testNoCatchCallableWrapperException() {
+    WrapperException exception = assertThrows(
+      WrapperException.class,
+      () -> noCatch(() -> new URL(";"), WrapperException.class)
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testNoCatchCallableCustomWrapperException() throws Exception {
+  public void testNoCatchCallableCustomWrapperException() {
     NoCatch.setDefaultWrapperException(RuntimeException.class);
-    expectedException.expect(RuntimeException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> new URL(";"));
+    RuntimeException exception = assertThrows(
+      RuntimeException.class,
+      () -> noCatch(() -> new URL(";"))
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
     NoCatch.setDefaultWrapperException(NoCatchException.class);
   }
 
   @Test
-  public void testNoCatchCallableInvalidWrapperException() throws Exception {
-    expectedException.expect(NoCatchException.class);
-    expectedException.expectCause(isA(MalformedURLException.class));
-
-    noCatch(() -> new URL(";"), InvalidWrapperException.class);
+  public void testNoCatchCallableInvalidWrapperException() {
+    NoCatchException exception = assertThrows(
+      NoCatchException.class,
+      () -> noCatch(() -> new URL(";"), InvalidWrapperException.class)
+    );
+    assertThat(exception.getCause(), instanceOf(MalformedURLException.class));
   }
 
   @Test
-  public void testIsUtilityClass() throws Exception {
+  public void testIsUtilityClass() {
     assertTrue(Modifier.isFinal(NoCatch.class.getModifiers()));
 
     Constructor<?>[] constructors = NoCatch.class.getDeclaredConstructors();
-    assertTrue(constructors.length == 1);
+    assertEquals(constructors.length, 1);
 
     Constructor<?> constructor = constructors[0];
     try {
